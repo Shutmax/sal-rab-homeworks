dedd89d15b9930afff010f3a40d08711b8523b3d
Max Grishenko <shutmax@rambler.ru>
2021-11-04 23:41:33 +0300

    qw

--- task_2/solution.js
+++ task_2/solution.js
@@ -16,6 +16,7 @@ function calcShipping(sum, min, shipping) {
     // если productsSum больше 0 и меньше freeShippingMinSum,
     // то shippingSum присвоить значение shippingPrice
 // то shippingSum присвоить значение shippingPrice
+fdres
 
     // Конец решения задания №2.1.
 
