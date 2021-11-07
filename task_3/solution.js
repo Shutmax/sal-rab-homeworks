a605dd6b2b1cde9cf2a14f9ad702254707cdace1
Max Grishenko <shutmax@rambler.ru>
2021-11-07 21:53:44 +0300

    7/11

--- task_3/solution.js
+++ task_3/solution.js
@@ -9,20 +9,14 @@ function renderCartItem(item) {
     // title - наименование
     // price - цена
     // count - количество
+    console.log(product.id, product.title, product.count, product.price);
     // сформируйте строку itemCountText вида:
     // количество × цена ₽ = стоимость ₽
     // например,
     // 5 × 500.00 ₽ = 2500 ₽
+    let sum = product.count*product.price;
+    console.log(sum);
     // 1. Создайте переменную itemCountText, равную пустой строке
-    let item = {id: id, title: title, count: count, price: price};
-    let sum = count*price;
-    let itemCountText = ``;
-    itemCountText += ` × `;
-    itemCountText += price;
-    itemCountText += ` ₽ = `;
-    itemCountText += sum;
-    itemCountText += ` ₽`;
-    console.log(itemCountText);
     // 2. Присвойте переменной itemCountText результат склеивания itemCountText и количества
     // 3. Присвойте переменной itemCountText результат склеивания itemCountText и ` × `
     // 4. Присвойте переменной itemCountText результат склеивания itemCountText и цены
@@ -30,7 +24,19 @@ function renderCartItem(item) {
     // 6. Создайте переменную sum и присвойте ей значение произведения количества и цены
     // 7. Присвойте переменной itemCountText результат склеивания itemCountText и sum
     // 8. Присвойте переменной itemCountText результат склеивания itemCountText и ` ₽`
-
+    let itemCountText = ``;
+    itemCountText += product.count;
+    console.log(itemCountText);
+    itemCountText += ` × `;
+    console.log(itemCountText);
+    itemCountText += product.price;
+    console.log(itemCountText);
+    itemCountText += ` ₽ = `;
+    console.log(itemCountText);
+    itemCountText += sum;
+    console.log(itemCountText);
+    itemCountText += ` ₽`;
+    console.log(itemCountText);
     // Конец решения задания №3.1.
 
     return `<div class="cart-item"><div>${product.title}</div><div>${itemCountText}</div></div>`;
