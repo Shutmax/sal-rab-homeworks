7e5d7fa174ca9687ee60cfe734b8ca2e51b6b844
Max Grishenko <shutmax@rambler.ru>
2021-11-14 21:56:34 +0300

    14/11

--- task_4/solution.js
+++ task_4/solution.js
@@ -2,14 +2,15 @@
 
 function parseProducts(json) {
     // Аргументом функции является JSON
-
+let json = JSON.parse(json);
     // Преобразуйте строку json, переданную как аргумент функции,
     // в объект с помощью функции JSON.parse(json)
     // и запишите в переменную data
     // Верните как результат функции свойство products объекта data
-}
-function renderProductsCards(json) {
+    let data = JSON.parse(json);
+    let products = data.products;
     
+    return products;
 }
 // Напишите функцию renderProductsCards(json)
 // Аргументом функции является JSON
@@ -18,3 +19,13 @@ function renderProductsCards(json) {
 // Запишите в переменную length значение свойства products.length
 // Напишите цикл, в котором перебираете все products от 0 до (length - 1)
 // и выполняете addProduct с каждым элементом массива products (передавая элемент массива как аргумент)
+function renderProductsCards(json) {
+    clearProducts();
+    
+    let products = parseProducts(json);
+    let length = products.length;
+
+    for (let i = 0; i < length; i ++ ){
+       addProduct(products[i]);
+    }
+}
