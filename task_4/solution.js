c57b37200517b00565d9917c5275d34f891b2206
Max Grishenko <shutmax@rambler.ru>
2021-11-16 23:17:37 +0300

    16.11

--- task_4/solution.js
+++ task_4/solution.js
@@ -2,7 +2,7 @@
 
 function parseProducts(json) {
     // Аргументом функции является JSON
-let json = JSON.parse(json);
+
     // Преобразуйте строку json, переданную как аргумент функции,
     // в объект с помощью функции JSON.parse(json)
     // и запишите в переменную data
